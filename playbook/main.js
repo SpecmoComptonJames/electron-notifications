f48d35da363647e67270cba0e5edb31fc793ae91
SpecmoComptonJames <james.compton@specmo.com>
2021-02-09 13:18:41 -0500

    update for electron 11.2.2 update for node v12.18.* update fix issues with window contract changes for blank windpws

--- playbook/main.js
+++ playbook/main.js
@@ -3,9 +3,10 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow = null
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL('file://' + __dirname + '/playbook.html')
+  mainWindow = new BrowserWindow({ width: 800, height: 600, webPreferences: { nodeIntegration: true, enableRemoteModule: true , contextIsolation: false,} })
+  mainWindow.loadURL('file://' + __dirname + '/playbook.html');
+  mainWindow.webContents.openDevTools()
   mainWindow.on('closed', () => {
-    mainWindow = null
+    mainWindow = null;
   })
 })
